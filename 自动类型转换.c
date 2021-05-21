9f954a5e033c7d5b27108510c75036a8f35c9377
zoesamaii <opxeva@gmail.com>
2021-05-21 09:53:39 +0800

    Create 自动类型转换.c

---
+++ 自动类型转换.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char c = 'a';
+    int n =c;         //将c赋值给n
+    float f =n;      //将c赋值给f
+    double d =f;      //将c赋值给d
+    printf("%d\n",n);
+    printf("%f\n",f);
+    printf("%lf\n",d);
+    return 0;    
+}
+
